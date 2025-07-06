0a0db5fb10e9725b756e261210af7ea44ee47d4c
RSourceCode <rahulsingh654932@gmail.com>
2025-07-06 21:32:15 +0530

    Edited Complete_Solver

--- Complete_Solver.py
+++ Complete_Solver.py
@@ -134,16 +134,6 @@ for i in range(9):
     sudoku.append(sudoku_digit_row)
     print(sudoku_digit_row)
 
-sudoku = [[5, 3, 0, 0, 7, 0, 0, 0, 0],   
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],   
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],   
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],   
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],   
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],   
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],   
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],   
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]]   
-
 ## Helper function to check if the value put is a valid/Solved one or not
 def isValid(sudoku, i, j, k):
     for l in range(9):
